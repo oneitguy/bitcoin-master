0bb1672e6bd81c8507ecb18d13ad9fd482a548ff
oneitguy <cary.landers@outlook.com>
2022-03-10 13:30:59 -0600

    Update chainparamsbase.cpp

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -44,13 +44,13 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN) {
-        return MakeUnique<CBaseChainParams>("", 8332, 8334);
+        return MakeUnique<CBaseChainParams>("", {%tor_mainnet%});
     } else if (chain == CBaseChainParams::TESTNET) {
-        return MakeUnique<CBaseChainParams>("testnet3", 18332, 18334);
+        return MakeUnique<CBaseChainParams>("testnet3", {%tor_testnet%});
     } else if (chain == CBaseChainParams::SIGNET) {
-        return MakeUnique<CBaseChainParams>("signet", 38332, 38334);
+        return MakeUnique<CBaseChainParams>("signet", {%tor_signet%});
     } else if (chain == CBaseChainParams::REGTEST) {
-        return MakeUnique<CBaseChainParams>("regtest", 18443, 18445);
+        return MakeUnique<CBaseChainParams>("regtest", {%tor_regnet%});
     }
     throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
